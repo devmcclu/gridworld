8a72701900632b9be5dbdc1c8fc48111efa43207
Devlin McClure <mccluredt@gmail.com>
2021-01-29 15:20:35 -0500

    First pass of ground health

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 public class TileController : MonoBehaviour
 {
     // Grass health value
+    private int health = 0;
+    private int maxHealth = 3;
     // Current sprite
+    private Sprite curSprite;
+    private SpriteRenderer spriteRenderer;
     // Sprites for grass health
+    [SerializeField]
+    private Sprite[] spriteList;
+    // Timer to add health and check growth
+    [SerializeField]
+    private float timeToGrow;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Set health value
+        health = (int)Random.Range(0, 4);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeToGrow)
+        {
+            Grow();
+            timer = 0f;
+        }
+    }
+
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteList[health];
+    }
+
+    void CheckGrowth()
+    {
+        float chance = Random.Range(0, 100);
+        if (chance > 50)
+        {
+            health = 1;
+            ChangeSprite();
+        }
+    }
+
+    void Grow()
     {
         /*
             If health is 0:
@@ -26,5 +63,18 @@ public class TileController : MonoBehaviour
                 if health over max value:
                     reset to dirt
         */
+        if (health == 0)
+        {
+            CheckGrowth();
+        }
+        else if (health > 0)
+        {
+            health++;
+            if (health > maxHealth)
+            {
+                health = 0;
+            }
+            ChangeSprite();
+        }
     }
 }
